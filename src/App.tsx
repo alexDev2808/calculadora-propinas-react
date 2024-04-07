1e1117a4b614e0f8b651afe46c9e4db3a4c3ef38
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-06 18:03:23 -0600

    Iniciando proyecto, agregando TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <header className="bg-violet-700 py-5">
+        <h1 className="text-center text-4xl font-bold text-white">Calculadora de Propinas y Consumo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
