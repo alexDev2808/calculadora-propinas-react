7d0ad48fc6afeaa1ad9fb4de6d72e8183c0531b3
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-07 10:19:17 -0600

    Formateando el consumo y mostrandolo en pantalla

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers"
 import { OrderItem } from "../types"
 
 type OrderContentsProps = {
@@ -13,12 +14,29 @@ export default function OrderContents({ order } : OrderContentsProps ) {
             { order.length === 0
                 ? <p className="text-center">La orden esta vacia</p>
                 : (
-                    order.map( item => (
-                        <div key={ item.id }>
-                            <p>
-                                { item.name }
-                            </p>
+                    order.map( item => ( 
+
+                        <div
+                            className="flex justify-between items-center border-t border-green-200 py-5 last-of-type:border-b" 
+                            key={ item.id }
+                        >
+                            <div>
+                                <p className="text-lg">
+                                    { item.name } - { formatCurrency( item.price ) }
+                                </p>
+                                <p className="font-bold">
+                                    Cantidad: { item.quantity } - { formatCurrency( item.price * item.quantity ) }
+                                </p>
+                            </div>
+
+                            <button
+                                className="bg-red-600 h-8 w-8 rounded-full text-white font-bold"
+                            >
+                                X
+                            </button>
+
                         </div>
+
                     ))
                 )
             }
